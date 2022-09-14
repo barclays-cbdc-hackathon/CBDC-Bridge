1c36d5999f3b8d95b0b45ce0b7ba39698ffca42f
Lazar Iliev <lazar.iliev@industria.tech>
2022-09-14 17:57:48 +0300

    fix: remove duplicated classes

--- cbdc-bridge/src/main/kotlin/com/cbdc/industria/tech/bridge/data/OpenBankingAccountInformation.kt
+++ cbdc-bridge/src/main/kotlin/com/cbdc/industria/tech/bridge/data/OpenBankingAccountInformation.kt
@@ -1,6 +1,5 @@
 package com.cbdc.industria.tech.bridge.data
 
-import com.cbdc.industria.tech.bridge.enums.BankingEntityType
 import com.cbdc.industria.tech.bridge.views.AccountAccessConsentView
 import net.corda.core.serialization.CordaSerializable
 
@@ -9,21 +8,6 @@ data class OpenBankingAccountAccessConsentCreationRequestBody(
     val requestingPartyId: RequestingPartyId,
     val bankingEntityWhereRequestingPartyIsRegisteredRef: BankingEntityWhereRequestingPartyIsRegisteredRef,
     val requestingBankingEntityRef: RequestingBankingEntityRef
-)
-
-@CordaSerializable
-data class RequestingPartyId(val partyId: Long)
-
-@CordaSerializable
-data class BankingEntityWhereRequestingPartyIsRegisteredRef(
-    val bankingEntityType: BankingEntityType,
-    val bankingEntityId: Long
-)
-
-@CordaSerializable
-data class RequestingBankingEntityRef(
-    val bankingEntityType: BankingEntityType,
-    val bankingEntityId: Long
 )
 
 @CordaSerializable
