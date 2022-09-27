c7604e188904a6a28b014e6d43177cbf03df0253
vjuge <vincent.juge@gmail.com>
2022-09-27 11:54:53 +0200

    fix client can't find flow (wip)

---
+++ clients/src/main/kotlin/com/cbdc/industria/tech/webserver/Controller.kt
@@ -0,0 +1,37 @@
+package com.cbdc.industria.tech.webserver
+
+import com.cbdc.industria.tech.flows.CheckPings
+import com.cbdc.industria.tech.webserver.rpc.NodeRPCConnection
+import net.corda.core.messaging.startFlow
+import net.corda.core.utilities.getOrThrow
+import org.hibernate.annotations.Check
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Controller {
+
+    @Autowired
+    lateinit var node : NodeRPCConnection
+
+    @GetMapping("/")
+    fun hello(): String {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/ping")
+    fun ping() : List<String>{
+        return try {
+            //node.proxy.startFlowDynamic(CheckPings::class.java).returnValue.getOrThrow()
+            node.proxy.startFlow(::CheckPings).returnValue.getOrThrow()
+
+//            ResponseEntity.status(HttpStatus.CREATED).body("Transaction id ${signedTx.id} committed to ledger.\n")
+
+        } catch (ex: Throwable) {
+            listOf<String>(ex.toString())
+//            logger.error(ex.message, ex)
+//            ResponseEntity.badRequest().body(ex.message!!)
+        }
+    }
+}
